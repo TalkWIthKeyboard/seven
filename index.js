0b86d1df105c703038f46cbffcdbdeaa4d6ef535
宋伟 <610347922@qq.com>
2017-04-20 20:46:10 +0800

    1. 添加权限系统（update还有bug） 2. 精简API，把中间件的权限收回到index中

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 let pub = {};
 let _ = require('underscore');
+let fs = require('fs');
+let path = require('path');
 let Promise = require('promise');
 let express = require('express');
 let colors = require('colors');
@@ -11,16 +13,24 @@ let router = express.Router();
 let _model = require('./lib/creator').modelCreator;
 let _api = require('./lib/creator').apiCreator;
 let _url = require('./lib/creator').urlCreator;
+let authorityFilter = require('./lib/authority').authorityFilter;
 let apiMap = require('./lib/factory/confFactory').schemaGeneralConf.apiMapping;
 
 /**
+ * 错误中间件
+ */
+pub.errorHandler = require('./lib/error').errorHandler;
+
+/**
  * 创造器
+ * @param app
  * @param schemaPath
- * @param rule
- * @param scb 成功回调
  * @param fcb 失败回调
  */
-pub.creator = (schemaPath, rule, scb, fcb) => {
+pub.creator = (app, schemaPath, fcb) => {
+  let configure = require(path.join(__dirname, '..', '.seven.json'));
+  let rule = configure.rule;
+  let authority = configure.authority;
   let _rule = {};
 
   Promise.resolve(schemaPath)
@@ -28,17 +38,18 @@ pub.creator = (schemaPath, rule, scb, fcb) => {
     .then(_api)
     .then(_url)
     .then((urlObj) => {
+
       // 处理规则
       _.each(_.keys(urlObj), (key) => {
         _rule[key + 's'] = {};
         _.each(_.keys(apiMap), (func) => {
           _rule[key + 's'][func] = true;
-        });
+        })
       });
       _.mapObject(rule, (value, key) => {
         _.mapObject(value, (_value, _key) => {
           _rule[key + 's'][_key] = _value;
-        });
+        })
       });
 
       // 生成映射
@@ -48,46 +59,48 @@ pub.creator = (schemaPath, rule, scb, fcb) => {
           if (_keys === 'model') return;
           let func = _rule[model.collection.name][_value.funcType];
           if (func) {
-            if (_value.type === 'get') {
-              // GET
-              if (_value.funcType === 'Retrieve')
-                router.get(_value.url, (req, res, next) => {
-                  _value.func(req, res, model, func.paramsList || ['id'], next);
-                });
+            // GET
+            if (_value.funcType === 'Retrieve')
+              router.get(_value.url, (req, res, next) => {
+                _value.func(req, res, model, func.paramsList || ['id'], next)
+              });
 
-              // Pagination
-              if (_value.funcType === 'Pagination')
-                router.get(_value.url, (req, res, next) => {
-                  _value.func(req, res, model, func.paramsList || ['page'], next);
-                });
-            }
+            // Pagination
+            if (_value.funcType === 'Pagination')
+              router.get(_value.url, (req, res, next) => {
+                _value.func(req, res, model, func.paramsList || ['page'], next)
+              });
 
             // Create
-            if (_value.type === 'post')
+            if (_value.funcType === 'Create')
               router.post(_value.url, (req, res, next) => {
                 func.bodyList
                   ? _value.func(req, res, model, func.bodyList, func.key || null, next)
-                  : _value.func(req, res, model, null, func.key || null, next);
+                  : _value.func(req, res, model, null, func.key || null, next)
               });
-
 
             // Delete
-            if (_value.type === 'delete')
+            if (_value.funcType === 'Delete')
               router.delete(_value.url, (req, res, next) => {
-                _value.func(req, res, model, func.paramsList || ['id'], next);
+                _value.func(req, res, model, func.paramsList || ['id'], next)
               });
 
-
             // Update
-            if (_value.type === 'put')
+            if (_value.funcType === 'Update')
               router.put(_value.url, (req, res, next) => {
                 func.bodyList
                   ? _value.func(req, res, model, func.paramsList || ['id'], func.bodyList, next)
-                  : _value.func(req, res, model, func.paramsList || ['id'], null, next);
+                  : _value.func(req, res, model, func.paramsList || ['id'], null, next)
               });
 
+            // Login
+            if (_value.funcType === 'Login'){
+              router.post(_value.url, (req, res, next) => {
+                _value.func(req, res, model, next);
+              });
+            }
           }
-        });
+        })
       });
 
       // 输出API
@@ -96,22 +109,21 @@ pub.creator = (schemaPath, rule, scb, fcb) => {
         console.log('\n', colors.gray(name));
         delete value.model;
         _.each(value, (_value) => {
-          if (_rule[name][_value.funcType])
+          if (_rule[name][_value.funcType]) {
             console.log('   ', _value.funcType.cyan, _value.type.green, _value.url.blue);
-
+          }
         });
       });
 
-      scb(router);
+      // 中间件
+      if (authority) app.use(authorityFilter());
+      app.use(router);
+      app.use(pub.errorHandler());
     })
     .catch((err) => {
       fcb(err);
     });
 };
 
-/**
- * 错误中间件
- */
-pub.errorHandler = require('./lib/error').errorHandler;
 
 module.exports = pub;
